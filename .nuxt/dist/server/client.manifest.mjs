7759a7a5a1984160f55161026a1034bc8f89845e
Fraser McTaggart <fraser@mctweb.co.uk>
2022-04-14 22:27:33 +0100

    Add output after build

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,10 @@
+export default {
+  "node_modules/nuxt3/dist/app/entry.mjs": {
+    "file": "entry-933df398.mjs",
+    "src": "node_modules/nuxt3/dist/app/entry.mjs",
+    "isEntry": true,
+    "css": [
+      "entry.d1fbfbe7.css"
+    ]
+  }
+}
---
+++ .nuxt/prerender/index.mjs
@@ -0,0 +1,13 @@
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/unenv/runtime/polyfill/fetch.node.mjs';
+export { l as localFetch } from './chunks/nitro-prerenderer.mjs';
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/h3/dist/index.mjs';
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/ohmyfetch/dist/node.mjs';
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/destr/dist/index.mjs';
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/radix3/dist/index.mjs';
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/unenv/runtime/fetch/index.mjs';
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/hookable/dist/index.mjs';
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/scule/dist/index.mjs';
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/defu/dist/defu.mjs';
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/ohash/dist/index.mjs';
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/unstorage/dist/index.mjs';
+import 'file://C:/Users/Fraser/Websites/2022/nuxt3-example/node_modules/ufo/dist/index.mjs';
